a197d6c53b1aafcaad137f98cad0231e4b4e9c87
Ahmarkalam <76834051+Ahmarkalam@users.noreply.github.com>
2025-01-31 00:35:12 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-// script.js
+// script.js// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB05Iz2lt09Wi0ciuoP3tOlnA-3h-pnAyw",
+  authDomain: "lawyerly-b366c.firebaseapp.com",
+  projectId: "lawyerly-b366c",
+  storageBucket: "lawyerly-b366c.firebasestorage.app",
+  messagingSenderId: "81871011557",
+  appId: "1:81871011557:web:07ee3c54e7830ee0501e0c",
+  measurementId: "G-1B86ZZ7D21"
+};
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
