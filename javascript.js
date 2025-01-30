1b37b595bdbf27330b929a91de712cd0087b119c
Ahmarkalam <76834051+Ahmarkalam@users.noreply.github.com>
2025-01-30 23:49:22 +0530

    Update javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your submission. We will contact you shortly.');
+    this.reset();
+});
